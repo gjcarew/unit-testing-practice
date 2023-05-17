050d36a829d6df9bda68d22b0521626cf9918229
Gavin Carew <gjcarew@gmail.com>
2023-05-17 15:09:01 -0700

    Finish unit testing practice

--- practice.js
+++ practice.js
@@ -1,9 +1,32 @@
 function findRemainder(n1, n2) {
     return n1 % n2;
 }
+function startGPS(current, future) {
+    if (current === future) {
+        return "You are already at the ".concat(current, "!  Would you like to go somewhere else?");
+    }
+    ;
+    return "Taking you from ".concat(current, " to ").concat(future, "!  Getting directions now!");
+}
+function createUser(fname, lname, bday, onlineStatus) {
+    if (onlineStatus === void 0) { onlineStatus = false; }
+    return {
+        firstName: fname,
+        lastName: lname,
+        birthDate: bday,
+        onlineStatus: onlineStatus
+    };
+}
+function makePizza(pizza, newTopping) {
+    pizza.cooked = true;
+    if (pizza.toppings.length < 5) {
+        pizza.toppings.push(newTopping);
+    }
+    return pizza;
+}
 module.exports = {
     findRemainder: findRemainder,
-    // startGPS,
-    // createUser,
-    // makePizza
+    startGPS: startGPS,
+    createUser: createUser,
+    makePizza: makePizza
 };
--- practice.ts
+++ practice.ts
@@ -2,11 +2,35 @@ function findRemainder(n1: number, n2: number): number {
     return n1 % n2 
 }
 
+function startGPS(current: string, future: string): string {
+    if (current === future) {
+        return `You are already at the ${current}!  Would you like to go somewhere else?`
+    };
+
+    return `Taking you from ${current} to ${future}!  Getting directions now!`
+}
+
+function createUser(fname: string, lname: string, bday: string, onlineStatus: boolean = false) {
+    return {
+        firstName: fname,
+        lastName: lname,
+        birthDate: bday,
+        onlineStatus: onlineStatus
+    }
+}
 
 
+
+function makePizza(pizza:{toppings: string[], cooked: boolean}, newTopping: string) {
+    pizza.cooked = true
+    if (pizza.toppings.length < 5) {
+        pizza.toppings.push(newTopping)
+    }
+    return pizza
+}
 module.exports = {
   findRemainder,
-  // startGPS,
-  // createUser,
-  // makePizza
+  startGPS,
+  createUser,
+  makePizza
 }
